68039ef3951f4b1b6b51e4794914eefab950ef9e
jerrynsl <ngsiliang5515@e.newera.edu.my>
2021-10-27 20:15:35 +0800

    dummy content

---
+++ eshop/contact.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- container -->
+    <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-sm-light bg-light ">
+        <div class="container-sm border ">
+          
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav justify-content-end ">
+              <li class="nav-item">
+                <a class="nav-link active text-primary" aria-current="page" href="home.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-primary" href="product_create.php">Create Product</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-primary" href="create_customer.php" >Create Customer</a>
+                
+              </li>
+              <li class="nav-item ">
+                <a class="nav-link text-primary" href="contact.php">Contact Us</a>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
+        <div class="page-header">
+            <h1>Contact us</h1>
+        </div>
+
+
+        
+
+
+
+
+
+    </div>
+    <!-- end .container -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
+
+</html>
